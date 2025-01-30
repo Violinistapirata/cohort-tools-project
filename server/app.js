f83b6abcc51ffd05769b2214ac35c3936fbf938c
Eric Masip Vilalta <ericmasipvilalta@gmail.com>
2025-01-30 13:03:59 +0100

    Fixed dotenv implementation and logger variable and added comments in app.js

--- server/app.js
+++ server/app.js
@@ -1,23 +1,26 @@
+
+
+/* ----------------------- SERVER SET UP ----------------------- */
+
+
 // IMPORTS
-const express = require("express");
-const morgan = require("morgan");
-const cookieParser = require("cookie-parser");
-const mongoose = require("mongoose");
-require("dotenv").config();
+const express = require("express"); //<-- To create the server
+const logger = require("morgan"); //<-- To console.log requests in the server terminal
+const CORS = require("cors"); //<-- To allow the front end to make requests to the server
+const cookieParser = require("cookie-parser"); //<-- ???
+const mongoose = require("mongoose"); //<-- To connect to MongoDB
+
+// STATIC DATA (SCHEMA MODELS)
+// Devs Team - Import the provided files with JSON data of students and cohorts here: <-- THIS CAN BE DELETED
 const Cohort = require("./models/Cohort.model");
 const Student = require("./models/Student.model");
-const CORS = require("cors");
 
 // .ENV VARIABLES
+require("dotenv").config();
 const MONGODB_URI = process.env.MONGODB_URI;
-const PORT = 5005;
+const PORT = process.env.PORT; //<--PORT=5005
 
-// STATIC DATA
-// Devs Team - Import the provided files with JSON data of students and cohorts here:
-// ...
-
-
-// INITIALIZE EXPRESS APP - https://expressjs.com/en/4x/api.html#express
+// INITIALIZE EXPRESS APP - https://expressjs.com/en/4x/api.html#express  <-- THIS URL CAN BE DELETED
 const app = express();
 
 // CONNECTION TO THE DDBB
@@ -26,28 +29,38 @@ mongoose
 .then((res) => console.log(`Connected to Mongo! Database name: "${res.connections[0].name}"`))
 .catch((err) => console.error("Error connecting to mongo", err))
 
-console.log(process.env.MONGODB_URI);
+// console.log(process.env.MONGODB_URI); <-- THIS CONSOLE LOG SHOULD BE DELETED
 
-// MIDDLEWARE
+
+
+/* ----------------------- MIDDLEWARE ----------------------- */
+
+
 // Research Team - Set up CORS middleware here:
-// ...
-app.use(express.json());
-app.use(morgan("dev"));
-app.use(express.static("public"));
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
+/* app.use(CORS({origin: ['http://localhost:5173/']})); //<-- client PORT (npm run dev in client side) */
 app.use(CORS());
+app.use(express.json()); //<-- Parses request query strings to json format
+app.use(logger("dev"));
+app.use(express.static("public")); //<-- configures access to the static files in this directory
+app.use(express.urlencoded({ extended: false })); //<-- ???
+app.use(cookieParser()); //<-- ???
 
 
-// ROUTES - https://expressjs.com/en/starter/basic-routing.html
-// Devs Team - Start working on the routes here:
-// ...
-//COHORT ROUTES
 
+/* ----------------------- COHORTS ROUTES ----------------------- */
+
+
+// ROUTES - https://expressjs.com/en/starter/basic-routing.html <-- THIS CAN BE DELETED
+// Devs Team - Start working on the routes here:                <-- THIS CAN BE DELETED
+
+// GET
+
+// Show the API documentation page
 app.get("/docs", (req, res) => {
   res.sendFile(__dirname + "/views/docs.html");
 });
 
+// Show the info of all the cohorts 
 app.get("/api/cohorts",(req, res)=>{
   Cohort.find().then((cohorts) => {
     res.json(cohorts);
@@ -58,6 +71,7 @@ app.get("/api/cohorts",(req, res)=>{
   
 });
 
+// Show only the info of the specified cohort 
 app.get("/api/cohorts/:cohortId",(req, res)=>{
   Cohort.findById(req.params.cohortId)
   .then((cohort)=>{
@@ -69,8 +83,9 @@ app.get("/api/cohorts/:cohortId",(req, res)=>{
   }); 
 });
 
-//  COHORT POST ROUTES
+// POST
 
+// Create a new cohort with the info provided in the request body
 app.post("/api/cohorts", (req, res) => {
   Cohort.create(req.body)
     .then((cohort) => {
@@ -81,8 +96,9 @@ app.post("/api/cohorts", (req, res) => {
     });
 });
 
-// COHORT PUT ROUTES
+// PUT
 
+// Update the specified existing cohort with the info provided in the request body
 app.put("/api/cohorts/:cohortId", (req, res) => {
   Cohort.findByIdAndUpdate(req.params.cohortId, req.body, { new: true })
     .then((cohort) => {
@@ -93,8 +109,9 @@ app.put("/api/cohorts/:cohortId", (req, res) => {
     });
 });
 
-// COHORT DELETE ROUTES
+// DELETE
 
+// Delete the specified existing cohort
 app.delete("/api/cohorts/:cohortId", (req, res) => {
   Cohort.findByIdAndDelete(req.params.cohortId)
     .then((cohort) => {
@@ -107,8 +124,12 @@ app.delete("/api/cohorts/:cohortId", (req, res) => {
 
 
 
-// STUDENTS ROUTES
+/* ----------------------- STUDENTS ROUTES ----------------------- */
 
+
+// GET
+
+// Show the info of all the students 
 app.get("/api/students", (req, res) => {
   Student.find()
     .populate("cohort")
@@ -120,7 +141,7 @@ app.get("/api/students", (req, res) => {
     });
 });
 
-
+// Show only the info of the students that belong in the specified cohort
 app.get("/api/students/cohort/:cohortId",(req, res)=>{
   Student.find({cohort: req.params.cohortId})
   .populate("cohort")
@@ -132,7 +153,7 @@ app.get("/api/students/cohort/:cohortId",(req, res)=>{
   });
 });
 
-
+// Show only the info of the specified student
 app.get("/api/students/:studentId",(req, res)=>{
   Student.findById(req.params.studentId)
   .populate("cohort")
@@ -144,9 +165,10 @@ app.get("/api/students/:studentId",(req, res)=>{
   });
 });
 
-// STUDENT POST ROUTES
 
+// POST
 
+// Create a new student with the info provided in the request body
 app.post("/api/students", (req, res) => {
   Student.create(req.body)
     .then((student) => {
@@ -157,8 +179,10 @@ app.post("/api/students", (req, res) => {
     });
 });
 
-// STUDENT PUT ROUTES
 
+// PUT
+
+// Update the specified existing student with the info provided in the request body
 app.put("/api/students/:studentId", (req, res) => {
   Student.findByIdAndUpdate(req.params.studentId, req.body, { new: true })
     .then((student) => {
@@ -169,8 +193,10 @@ app.put("/api/students/:studentId", (req, res) => {
     });
 });
 
-// STUDENT DELETE ROUTES
 
+// DELETE
+
+// Delete the specified existing student
 app.delete("/api/students/:studentId", (req, res) => {
   Student.findByIdAndDelete(req.params.studentId)
     .then((student) => {
@@ -182,7 +208,10 @@ app.delete("/api/students/:studentId", (req, res) => {
 });
 
 
-// START SERVER
+
+/* ----------------------- START THE SERVER ----------------------- */
+
+
 app.listen(PORT, () => {
   console.log(`Server listening on port ${PORT}`);
 });
